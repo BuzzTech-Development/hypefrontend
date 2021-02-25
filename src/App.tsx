a0c06e20876f2c3d6f1a4139b2508f1a1d491fbd
David Adams <dadams2212@gmail.com>
2021-02-24 21:20:06 -0500

    Fix App component and imports, and add App.css to the project so that antd css is applied to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Button} from "antd";
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Button>HYPE</Button>
     </div>
   );
 }
