d1e6188090fcad66673a73954c1feac1b556a9c0
David Adams <dadams2212@gmail.com>
2021-02-24 23:06:57 -0500

    Move redux directory under src/. Create a simple slice of the state for user information in redux/userSlice.ts.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { logger } from 'redux-logger';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import userSlice from "./userSlice";
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        user: userSlice,
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
@@ -13,3 +16,5 @@ export type AppDispatch = typeof store.dispatch
 // Create typed useDispatch and useSelector hooks
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default store
