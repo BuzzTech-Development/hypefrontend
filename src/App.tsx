17b351d4f5d35446ffef1e0b0da7d85006aa584d
David Adams <dadams2212@gmail.com>
2021-04-08 12:34:41 -0400

    Add placeholder Home scene that renders if the user is authenticated. Otherwise, App.tsx will render the Login scene instead

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import {Button} from "antd";
 import './App.css';
 import {useSelector} from "react-redux";
 import Login from "./scenes/Login";
+import {useAppSelector} from "./redux/store";
+import Home from "./scenes/Home";
 
 function App() {
-    return (
-        <Login />
-    );
+    const authenticated = useAppSelector((state) => state.user.authenticated);
+
+    return authenticated ? <Home /> : <Login />
 }
 
 export default App;
