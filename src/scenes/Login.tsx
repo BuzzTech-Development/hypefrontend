78ac59123c8e0790287f15eff0d9b941cc415b3e
David Adams <dadams2212@gmail.com>
2021-04-08 11:57:47 -0400

    Create login form with username and password fields in the Login scene

--- src/scenes/Login.tsx
+++ src/scenes/Login.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import {Button, Form, Input} from "antd";
 
 const Login = () => {
-    return <h2>Login</h2>;
+    return (
+        <Form>
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, }]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 }
 
 export default Login
