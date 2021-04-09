f24d11ec034c0baed627db776cd153b0744095af
David Adams <dadams2212@gmail.com>
2021-04-09 13:33:29 -0400

    Create basic home layout with collapsable menu sidebar with a few placeholder menu items

--- src/scenes/Home.tsx
+++ src/scenes/Home.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import {Layout, Menu} from 'antd';
+import React, {useState} from 'react';
+import {CalendarOutlined, HomeOutlined} from "@ant-design/icons";
 
 const Home = () => {
-    return <h2>Home</h2>;
+    const [menuCollapsed, setMenuCollapsed] = useState(false);
+
+    const onCollapse = (collapsed: boolean) => setMenuCollapsed(collapsed);
+
+    return (
+        <Layout style={{ minHeight: "100vh", }}>
+            <Layout.Sider collapsible collapsed={menuCollapsed} onCollapse={onCollapse}>
+                <Menu theme="dark" mode="vertical" style={{ height: "100vh", position: "absolute", width: "100%" }}>
+                    <Menu.Item icon={<HomeOutlined />}>Home</Menu.Item>
+                    <Menu.Item icon={<CalendarOutlined />}>Calendar</Menu.Item>
+                </Menu>
+            </Layout.Sider>
+            <Layout>
+                <Layout.Header style={{ padding: 0 }}>
+
+                </Layout.Header>
+                <Layout.Content>
+
+                </Layout.Content>
+                <Layout.Footer style={{ textAlign: "center" }}>HYPE</Layout.Footer>
+            </Layout>
+        </Layout>
+    )
 }
 
 export default Home;
