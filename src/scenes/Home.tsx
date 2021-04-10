4fe3a2ab0724eb356bc36a5560b3318a0264fb43
David Adams <dadams2212@gmail.com>
2021-04-10 11:50:09 -0400

    Add placeholder header title

--- src/scenes/Home.tsx
+++ src/scenes/Home.tsx
@@ -4,20 +4,26 @@ import {CalendarOutlined, HomeOutlined} from "@ant-design/icons";
 
 const Home = () => {
     const [menuCollapsed, setMenuCollapsed] = useState(false);
-
     const onCollapse = (collapsed: boolean) => setMenuCollapsed(collapsed);
+
+    const headerStyle = { height: "50px", color: "white", }
 
     return (
         <Layout style={{ minHeight: "100vh", }}>
             <Layout.Sider collapsible collapsed={menuCollapsed} onCollapse={onCollapse}>
-                <Menu theme="dark" mode="vertical" style={{ height: "100vh", position: "absolute", width: "100%" }}>
+                <div style={{ textAlign: "center", ...headerStyle }}>Hype Logo Here</div>
+                <Menu
+                    theme="dark"
+                    mode="vertical"
+                    style={{ height: "100vh", position: "absolute", width: "100%", }}
+                >
                     <Menu.Item icon={<HomeOutlined />}>Home</Menu.Item>
                     <Menu.Item icon={<CalendarOutlined />}>Calendar</Menu.Item>
                 </Menu>
             </Layout.Sider>
             <Layout>
-                <Layout.Header style={{ padding: 0 }}>
-
+                <Layout.Header style={{ padding: 0, textAlign: "center", ...headerStyle }}>
+                    <div>Header Title</div>
                 </Layout.Header>
                 <Layout.Content>
 
