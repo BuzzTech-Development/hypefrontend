906f95d722fe617e4aaed51978079123fa760207
David Adams <dadams2212@gmail.com>
2021-04-12 14:09:25 -0400

    Remove unused imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {Button} from "antd";
 import './App.css';
-import {useSelector} from "react-redux";
 import Login from "./scenes/Login";
 import {useAppSelector} from "./redux/store";
 import Home from "./scenes/Home";
