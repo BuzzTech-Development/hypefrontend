527d4a04f446be72dcac9fb315bc3e1daa87575f
Ryan Helgason <helgasonryan@gmail.com>
2021-09-21 22:10:43 -0400

    Auth context routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import AuthContext from './context/AuthContext';
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 import NavBar from './scenes/NavBar';
 import Home from "./scenes/Home";
 import Login from "./scenes/Login";
@@ -11,35 +11,38 @@ import Progress from 'scenes/Progress';
 import Account from 'scenes/Account';
 
 import './App.css';
-import { Layout } from 'antd';
 
-function App() {
+function App(props: any) {
     const user = useContext(AuthContext);
 
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/login">
-                    <NavBar content={<Login />} />
+                    {user.token ? <Redirect to="/home" /> : <Login /> }
+                    <Login />
                 </Route>
-                <Route path="/announcements">
-                    <NavBar content={<Announcements />} />
-                </Route>
-                <Route path="/calendar">
-                    <NavBar content={<Calendar />} />
-                </Route>
-                <Route path="/assignments">
-                    <NavBar content={<Assignments />} />
-                </Route>
-                <Route path="/progress">
-                    <NavBar content={<Progress />} />
-                </Route>
-                <Route path="/account">
-                    <NavBar content={<Account />} />
-                </Route>
-                <Route path="/">
-                    <NavBar content={<Home />} />
-                </Route>
+
+                {!user.token ? <Redirect to="/login" /> : <>
+                    <Route path="/announcements">
+                        <NavBar content={<Announcements />} />
+                    </Route>
+                    <Route path="/calendar">
+                        <NavBar content={<Calendar />} />
+                    </Route>
+                    <Route path="/assignments">
+                        <NavBar content={<Assignments />} />
+                    </Route>
+                    <Route path="/progress">
+                        <NavBar content={<Progress />} />
+                    </Route>
+                    <Route path="/account">
+                        <NavBar content={<Account />} />
+                    </Route>
+                    <Route path="/">
+                        <NavBar content={<Home />} />
+                    </Route>
+                </>}
             </Switch>
         </BrowserRouter>
     )
